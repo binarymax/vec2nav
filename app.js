0c94b2d3d6c497cab26013dc59450d23eba0abd5
Max Irwin <max_irwin@yahoo.com>
2015-10-15 17:37:22 +0100

    Native word2vec

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var config = require('./lib/config');
+
 var routes = require('./lib/routes');
 
 var app = express();
--- controllers/words.js
+++ controllers/words.js
@@ -1,14 +1,14 @@
-var w2v = require('word2vec');
+var w2v = require('word2vec-native');
 var phs = require('../lib/phrases');
-var vec;
 
-w2v.loadModel(process.cwd() + "/data/vectors.bin", function(err, model){ 
+w2v.open(GLOBAL.vec2nav.vectors, function(err,data){ 
 	if(err) throw err;
-	vec = model; 
+	console.log(" └──  Vectors loaded ("+data.words+" words)");
 });
 
-phs.load(process.cwd() + "/data/phrases.txt", function(err){ 
+phs.load(GLOBAL.vec2nav.phrases, function(err){ 
 	if(err) throw err;
+	console.log(" └──  Phrases loaded");
 });
 
 var Words = module.exports = {};
@@ -17,7 +17,9 @@ Words.GetSimilar = function(data,callback) {
 
 	var word = data.word.toLowerCase();
 
-	callback(null,vec.mostSimilar(word,20));
+	var words = w2v.get(word).words;
+
+	callback(null,words);
 
 };
 
